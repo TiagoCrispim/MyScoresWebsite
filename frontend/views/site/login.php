b912f30401a7f65b59ccd6b282f594b80b6c6bae
TiagoCrispim <2170386@my.ipleiria.pt>
2018-12-06 12:31:35 +0000

    Não Registado?

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -26,6 +26,10 @@ $this->params['breadcrumbs'][] = $this->title;
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
                 <div style="color:#999;margin:1em 0">
+                    Ainda não está registado? Registe-se <?= Html::a('aqui.', ['site/signup']) ?>.
+                </div>
+
+                <div style="color:#999;margin:1em 0">
                     If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
                 </div>
 
