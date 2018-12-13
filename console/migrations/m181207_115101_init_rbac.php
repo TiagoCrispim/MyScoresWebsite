3d785f6aaf3936157deb4d3163e1b28e78b9446b
rodasgam <rodasgam@gmail.com>
2018-12-13 11:16:48 +0000

    Update m181207_115101_init_rbac.php
    permissions and type of users created

--- console/migrations/m181207_115101_init_rbac.php
+++ console/migrations/m181207_115101_init_rbac.php
@@ -14,33 +14,46 @@ class m181207_115101_init_rbac extends Migration
     {
         $auth = Yii::$app->authManager;
 
-        // add "createPost" permission
-        $createPost = $auth->createPermission('createPost');
-        $createPost->description = 'Create a post';
-        $auth->add($createPost);
+        // add "createUser" permission
+        $createUser = $auth->createPermission('createUser');
+        $createUser->description = 'Create a normal user';
+        $auth->add($createUser);
 
         // add "updatePost" permission
-        $updatePost = $auth->createPermission('updatePost');
-        $updatePost->description = 'Update post';
-        $auth->add($updatePost);
+        $blockUser = $auth->createPermission('blockUser');
+        $blockUser->description = 'Block any user';
+        $auth->add($blockUser);
 
-        // add "author" role and give this role the "createPost" permission
-        $author = $auth->createRole('author');
-        $auth->add($author);
-        $auth->addChild($author, $createPost);
+        // add "updatePost" permission
+        $createAdmin = $auth->createPermission('createAdmin');
+        $createAdmin->description = 'create admins';
+        $auth->add($createAdmin);
+
+        // add "enterBackEnd" permission
+        $enterBackEnd = $auth->createPermission('enterBackEnd');
+        $enterBackEnd->description = 'defeining who can enter the back end of the site';
+        $auth->add($enterBackEnd);
+
+        $regular = $auth->createRole('regular');
+        $auth->add($regular);
+        $auth->addChild($regular, $createUser);
+
+
+        // add  role and give this role the "createPost" permission
+        $admin = $auth->createRole('admin');
+        $auth->add($admin);
+        $auth->addChild($admin, $blockUser);
+        $auth->addChild($admin, $enterBackEnd);
 
         // add "admin" role and give this role the "updatePost" permission
         // as well as the permissions of the "author" role
-        $admin = $auth->createRole('admin');
-        $auth->add($admin);
-        $auth->addChild($admin, $updatePost);
-        $auth->addChild($admin, $author);
+        $god = $auth->createRole('god');
+        $auth->add($god);
+        $auth->addChild($god, $createAdmin);
+        $auth->addChild($god, $admin);
 
-        // Assign roles to users. 1 and 2 are IDs returned by IdentityInterface::getId()
-        // usually implemented in your User model.
-        $auth->assign($author, 2);
-        $auth->assign($admin, 1);
 
+        
     }
 
     /**
