e67be6717d0ed1559ec41e2bd8d5518a62adf4de
TiagoCrispim <2170386@my.ipleiria.pt>
2019-01-08 05:07:32 +0000

    Updt index

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -11,17 +11,17 @@ $this->title = 'My Scores';
 
         <div class="row">
             <div class="col-lg-6">
-                <h2>Heading</h2>
+                <h1>MY SCORES</h1>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <h3>Em que consiste o Website?</h3>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p>O Website tem como principal objetivo incentivar a prática de futsal entre grupos de amigos.
+                    No website é possivel realizar todos os registos relacionados com cada jogo jogado como a data, hora e
+                    local onde foi realizado o jogo. É ainda possivel registar os membros de cada equipa assim como quem
+                    marcou os golos de cada uma das equipas.</p>
             </div>
             <div class="col-lg-6">
-                <h2>Entre</h2>
+                <h2>Registe-se</h2>
 
                 <p>Gere todos os teus jogos amigaveis! Basta entrar!</p>
 
