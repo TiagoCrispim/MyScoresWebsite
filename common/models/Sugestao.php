c7244923d8343214c13b569645a4d28cafc597d5
TiagoCrispim <2170386@my.ipleiria.pt>
2019-01-18 00:34:41 +0000

    Create sugestao.php

---
+++ common/models/Sugestao.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace common\models;
+
+use common\models\User;
+use Yii;
+
+/**
+ * This is the model class for table "sugestao".
+ *
+ * @property int $id
+ * @property int $id_user
+ * @property string $mensagem
+ */
+class Sugestao extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'sugestao';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['mensagem'], 'required'],
+            [['mensagem'], 'string'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'mensagem' => 'Mensagem:',
+        ];
+    }
+
+    /**
+     * relação da tabela sugestao com a tabela user
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::classname(),['id'=>'id_user']);
+    }
+}
