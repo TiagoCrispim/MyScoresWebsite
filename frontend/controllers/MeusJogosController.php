a923752e8acc6c62de24a674a168080ed69c92c3
rodasgam <rodasgam@gmail.com>
2019-01-25 22:28:08 +0000

    update meus jogos

--- frontend/controllers/MeusJogosController.php
+++ frontend/controllers/MeusJogosController.php
@@ -2,6 +2,7 @@
 namespace frontend\controllers;
 
 
+use common\models\Equipa;
 use common\models\GolosJogo;
 use common\models\User;
 use common\models\Jogo;
@@ -11,33 +12,59 @@ class MeusJogosController extends \yii\web\Controller
 {
     public function actionIndex(){
         $user_id=Yii::$app->user->getId();
-        $usernames1=array();
-        $usernames2=array();
+        $usernames1=array(array());
+        $usernames2=array(array());
+
 
         $golos_user= GolosJogo::findBySql('SELECT * FROM golos_jogo where id_user='.$user_id.' ORDER BY ID')->all();
-/*
+
         for($i=0;$i<count($golos_user);$i++) {
             $golos_equipa1[$i] = GolosJogo::findBySql('SELECT * FROM golos_jogo where id_equipa=' . $golos_user[$i]->id_equipa)->all();
+            $nome_equipa1[$i]= Equipa::findBySql('SELECT nome FROM equipa where  id=' . $golos_user[$i]->id_equipa)->all();
 
-            for($j=0;$j<count($golos_equipa1);$j++) {
-                $id_jogo=$golos_equipa1[$j]->id_jogo;
-                $usernames1[$i][$j]=User::findOne($golos_equipa1[$j]->id_user)->username;
-                $golos_equipa2=GolosJogo::findBySql('SELECT * FROM golos_jogo where id_jogo='.$id_jogo.' and id_equipa<>' . $golos_user[$i]->id_equipa)->all();
-                $usernames2[$i][$j]=User::findOne($golos_equipa2[$j]->id_user)->username;
-            }*/
+            for ($j = 0; $j < count($golos_equipa1); $j++) {
+                $id_jogo = $golos_equipa1[$i][0]['id_jogo'];
+                $golos_equipa2[$i] = GolosJogo::findBySql('SELECT * FROM golos_jogo where id_jogo=' . $id_jogo . ' and id_equipa<>' . $golos_user[$i]->id_equipa)->all();
+                $nome_equipa2[$i]= Equipa::findBySql('SELECT nome FROM equipa where  id=' . $golos_equipa2[$i][0]['id_equipa'])->all();
+            }
 
+            for($j=0;$j<=4;$j++){
+                $usernames1[$i][$j] = User::findOne($golos_equipa1[$i][$j]['id_user'])->username;
+                $usernames2[$i][$j] = User::findOne($golos_equipa2[$i][$j]['id_user'])->username;
+            }
+
+        }
+
+
+
+/*        $usernames3[0] = array();
+        $usernames3[1] = array();
+
+        for($i=0;$i<count($usernames1);$i++) {
+            $usernames3[0][$i] = $usernames1[$i];
+        }
+
+        for($i=0;$i<count($usernames2);$i++) {
+            $usernames3[1][$i] = $usernames2[$i];
+        }*/
 
        /*     for($k=0;$k<=count($golos_user);$k++){
                 return var_dump($golos_equipa1[$k]);
             }*/
         /*}*/
-        return var_dump($golos_user);
+        return $this->render('index', [
+
+                'nome_equipa1'=>$nome_equipa1,
+                'nome_equipa2'=>$nome_equipa2,
+                'golos_equipa1'=>$golos_equipa1,
+                'golos_equipa2'=>$golos_equipa2,
+                'usernames1'=>$usernames1,
+                'usernames2'=>$usernames2
 
 
 
-
-
-
+            ]
+        );
 
 
 
--- frontend/views/meus-jogos/index.php
+++ frontend/views/meus-jogos/index.php
@@ -5,3 +5,19 @@
  * Date: 25/01/2019
  * Time: 03:29
  */
+?>
+<?php for ($i=0;$i < count($golos_equipa1);$i++) {?>
+    <div class="card">
+
+        <span id="resultado1"> <?=$nome_equipa1[$i][0]['nome']?> </span> <span>VS</span> <span id="resultado2"> <?=$nome_equipa2[$i][0]['nome']?></span>
+
+
+        <?php for($j=0;$j<=count($usernames1);$j++){?>
+            <!--<div><?=$usernames1[$i][$j]?></div>-->?>
+
+        <?php}?>
+
+
+    </div>
+
+<?php}?>
